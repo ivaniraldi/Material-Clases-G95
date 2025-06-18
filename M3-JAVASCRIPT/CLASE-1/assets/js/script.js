c40881ec4588d6f18b7a815cf215a7df62a70f4d
ivaniraldi <iraldiban@gmail.com>
2025-06-17 22:37:45 -0300

    Modulo Javascript

---
+++ M3-JAVASCRIPT/CLASE-1/assets/js/script.js
@@ -0,0 +1,12 @@
+let edad = 26;
+let nombre = "Ivan";
+let apellido = "Iraldi";
+
+let botonTexto = "Hola desde javascript"
+
+// console.log(nombre + " " + apellido)
+
+// alert(nombre)
+ 
+let color = "rojo";
+let ivan = "minombre"
